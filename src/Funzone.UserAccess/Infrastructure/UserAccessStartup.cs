2aefe6dc9aa9ea292e9f62b51c175a5fa95ebbea
Bruce.l <bruce@sjfood.us>
2021-03-04 21:15:36 +0800

    Fix tests

---
+++ src/Funzone.UserAccess/Infrastructure/UserAccessStartup.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Funzone.BuildingBlocks.EventBus.Abstractions;
+using Funzone.UserAccess.Infrastructure.DataAccess;
+using Funzone.UserAccess.Infrastructure.Domain;
+using Funzone.UserAccess.Infrastructure.EventBus;
+using Microsoft.Extensions.Logging;
+
+namespace Funzone.UserAccess.Infrastructure
+{
+    public class UserAccessStartup
+    {
+        public static void Initialize(
+            string connectionString,
+            ILoggerFactory loggerFactory,
+            IEventBus eventBus)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterModule(new DataAccessModule(connectionString, loggerFactory));
+            containerBuilder.RegisterModule(new DomainModule());
+            containerBuilder.RegisterModule(new EventBusModule(eventBus));
+        }
+    }
+}
