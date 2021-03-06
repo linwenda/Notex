b4de85d94fcb95ff4f175b9c1b225133642b11b4
linwenda <a516483093@outlook.com>
2021-03-05 16:38:50 -0800

    WIP

---
+++ src/UserAccess/Funzone.UserAccess.Infrastructure/UserAccessStartup.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Funzone.BuildingBlocks.EventBus.Abstractions;
+using Funzone.UserAccess.Infrastructure.DataAccess;
+using Funzone.UserAccess.Infrastructure.Domain;
+using Funzone.UserAccess.Infrastructure.EventBus;
+using Microsoft.Extensions.Logging;
+using Serilog.Extensions.Logging;
+using ILogger = Serilog.ILogger;
+
+namespace Funzone.UserAccess.Infrastructure
+{
+    public class UserAccessStartup
+    {
+        public static void Initialize(
+            string connectionString,
+            ILogger logger,
+            IEventBus eventBus)
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            var loggerFactory = new LoggerFactory(new[]
+            {
+                new SerilogLoggerProvider(logger)
+            });
+
+            containerBuilder.RegisterModule(new DataAccessModule(connectionString, loggerFactory));
+            containerBuilder.RegisterModule(new DomainModule());
+            containerBuilder.RegisterModule(new EventBusModule(eventBus));
+        }
+    }
+}
---
+++ src/UserAccess/Funzone.UserAccess/Infrastructure/Domain/DomainModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Funzone.UserAccess.Domain.Users;
+using Funzone.UserAccess.Infrastructure.Domain.Users;
+
+namespace Funzone.UserAccess.Infrastructure.Domain
+{
+    public class DomainModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserRepository>()
+                .As<IUserRepository>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
