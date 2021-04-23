9bcf55089868e25eebf83be839d286eba4efa112
linwenda <a516483093@outlook.com>
2021-04-22 23:34:32 -0700

    Add api and fix compose

---
+++ src/Funzone.Api/Controllers/Zones/ZonesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Funzone.Application.Commands.Zones;
+using Funzone.Application.Commands.ZoneUsers;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Funzone.Api.Controllers.Zones
+{
+    [Route("api/zones")]
+    [ApiController]
+    public class ZonesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ZonesController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateZone(CreateZoneCommand command)
+        {
+            await _mediator.Send(command);
+            return Created("", "");
+        }
+
+        [HttpPost("join")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> JoinZone(JoinZoneCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+    }
+}
