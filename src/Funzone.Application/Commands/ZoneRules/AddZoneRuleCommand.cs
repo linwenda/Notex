56e39ba8d8b008f9cc45f12ed69f870150532149
Bruce.l <bruce@sjfood.us>
2021-04-25 22:08:56 +0800

    WIP

--- src/Funzone.Application/Commands/ZoneRules/AddZoneRuleCommand.cs
+++ src/Funzone.Application/Commands/ZoneRules/AddZoneRuleCommand.cs
@@ -4,17 +4,9 @@ namespace Funzone.Application.Commands.ZoneRules
 {
     public class AddZoneRuleCommand : ICommand<bool>
     {
-        public AddZoneRuleCommand(Guid zoneId, string title, string description, int sort)
-        {
-            ZoneId = zoneId;
-            Title = title;
-            Description = description;
-            Sort = sort;
-        }
-
-        public Guid ZoneId { get; }
-        public string Title { get; }
-        public string Description { get; }
-        public int Sort { get; }
+        public Guid ZoneId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Sort { get; set; }
     }
 }
