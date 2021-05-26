8e6749a119f5d8b5c89f268dab5f68535ba682ab
Bruce <bruce3309@outlook.com>
2021-05-26 01:44:18 -0700

    Aggregate

---
+++ src/Funzone.Domain/SeedWork/AggregateId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Funzone.Domain.SeedWork
+{
+    public abstract class AggregateId<T>
+        where T : AggregateRoot
+    {
+        protected AggregateId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+    }
+}
