d946f31838fe42183b0a4575c7806674fbf9bcbe
linwenda <bruce3309@outlook.com>
2021-09-11 01:16:58 -0700

    Add name props

--- src/MarchNote.Application/Users/Queries/UserDto.cs
+++ src/MarchNote.Application/Users/Queries/UserDto.cs
@@ -7,7 +7,8 @@ namespace MarchNote.Application.Users.Queries
         public Guid Id { get; set; }
         public DateTime RegisteredAt { get; set; }
         public string Email { get; set; }
-        public string NickName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Bio { get; set; }
         public string Avatar { get; set; }
         public bool IsActive { get; set; }
